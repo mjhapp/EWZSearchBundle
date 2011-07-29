7f3c2ff22e04be641071d3035b38cc083b6b7bb1
excelwebzone <excelwebzone@gmail.com>
2011-07-29 07:49:48 -0700

    Fix and add some phpDoc

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,28 +3,33 @@
 namespace EWZ\Bundle\SearchBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * configuration structure.
+ * This class contains the configuration information for the bundle
+ *
+ * This information is solely responsible for how the different configuration
+ * sections are normalized, and merged.
  */
-class Configuration
+class Configuration implements ConfigurationInterface
 {
     /**
-     * Generates the configuration tree.
+     * Generates the configuration tree builder.
      *
-     * @return \Symfony\Component\Config\Definition\ArrayNode The config tree
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
      */
-    public function getConfigTree()
+    public function getConfigTreeBuilder()
     {
-        $tree = new TreeBuilder();
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ewz_search');
 
-        $tree->root('ewz_search')
+        $rootNode
             ->children()
                 ->scalarNode('analyzer')->end()
                 ->scalarNode('path')->end()
             ->end()
         ;
 
-        return $tree->buildTree();
+        return $treeBuilder;
     }
 }
--- DependencyInjection/EWZSearchExtension.php
+++ DependencyInjection/EWZSearchExtension.php
@@ -5,20 +5,26 @@ namespace EWZ\Bundle\SearchBundle\DependencyInjection;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 
+/**
+ * EWZSearchExtension.
+ */
 class EWZSearchExtension extends Extension
 {
+    /**
+     * Loads the search configuration.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('lucene.xml');
 
-        $processor = new Processor();
         $configuration = new Configuration();
-
-        $config = $processor->process($configuration->getConfigTree(), $configs);
+        $config = $this->processConfiguration($configuration, $configs);
 
         $container->setParameter('lucene.analyzer', $config['analyzer']);
         $container->setParameter('lucene.index.path', $config['path']);
